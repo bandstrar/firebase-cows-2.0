a8b54696c222af2a1197e7ae86a5acef58ffa44d
bandstrar <rob.bandstra@gmail.com>
2020-09-22 21:28:27 -0500

    added functionality to add a cow and delete cows and farmers

--- src/javascripts/helpers/viewHelpers.js
+++ src/javascripts/helpers/viewHelpers.js
@@ -1,5 +1,6 @@
 import farmersView from '../components/views/farmersView';
 import cowsView from '../components/views/cowsView';
+import addCowView from '../components/views/addCowView';
 
 const viewHelper = (id) => {
   $('#app').html('');
@@ -8,12 +9,16 @@ const viewHelper = (id) => {
       return farmersView.farmersView();
     case 'cows-link':
       return cowsView.cowsView();
+    case 'add-cow-link':
+      return addCowView.addCowView();
     default:
       return console.warn('nothing clickded');
   }
 };
 
-const viewListener = () => {
+const viewListener = (view) => {
+  viewHelper(view);
+
   $('body').on('click', 'li.nav-item', (e) => {
     viewHelper(e.currentTarget.id);
   });
